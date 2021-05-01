e96cc92bb867b56004b5bb3d328150d0dd8e4f12
Sophie Stankus <75053520+SophieStankus@users.noreply.github.com>
2021-04-30 17:02:34 -0700

    Update Environment.java

--- Environment.java
+++ Environment.java
@@ -6,30 +6,38 @@ public class Environment {
 
     public class Variable {
         String name;
-        int value;
+        Value value;
 
-        public Variable(String str, int val) {
+        public Variable(String str, Value val) {
             this.name = str;
             this.value = val;
+        }
+
+        @Override 
+        public String toString() {
+            return "Name: " + this.name + " Val: " + this.value;
         }
     }
 
     public Environment() {
         this.parent = null;
     }
+    
+    public Environment(Environment parent) {
+        this.parent = parent;
+    }
 
     public void setParent(Environment parent) {
         this.parent = parent;
     }
 
-    public void addVariable(String str, int value) {
+    public void addVariable(String str, Value value) {
         Variable newVar = new Variable(str, value);
         variables.add(newVar);
     }
     
     public Boolean contains(String str) {
         for (int i = 0; i < variables.size(); i++) {
-            //System.out.println(variables.get(i).name);
             if (variables.get(i).name.equals(str)) {
                 return true;
             } 
@@ -37,7 +45,7 @@ public class Environment {
         return false;
     }
 
-    public void setVariable(String str, int val) {
+    public void setVariable(String str, Value val) {
         for (int i = 0; i < variables.size(); i++) {
             if (variables.get(i).name.equals(str)) {
                 variables.get(i).value = val;
@@ -46,12 +54,16 @@ public class Environment {
         }
     }
 
-    public int getValue(String str) {
+    public Value getValue(String str) {
         for (int i = 0; i < variables.size(); i++) {
             if (variables.get(i).name.equals(str)) {
                 return variables.get(i).value;
             } 
         }
-        return -1000000000;
+        return new Value();
+    }
+
+    public Environment getParent() {
+        return this.parent;
     }
 }
